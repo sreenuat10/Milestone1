af492bd30e7f761d8263c05431b2e0599945552f
DSRINIVASULU <DSRINIVASULU@DESKTOP-IOKJQ3J>
2020-06-11 20:56:35 +0530

    Example 3 in Language Basics.

---
+++ Milestone_1/src/languagebasics/Example3.java
@@ -0,0 +1,13 @@
+package languagebasics;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+
+		int i1=Integer.parseInt(args[0]);
+		int i2=Integer.parseInt(args[1]);
+        System.out.println("The sum of 10 and 20 is "+(i1+i2));
+        
+	}
+
+}
